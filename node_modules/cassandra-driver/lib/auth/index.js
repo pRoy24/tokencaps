5e2c0bfb0ffb0d85a604cdc409df59b5fa45b753
Pritam Roy <roypritam1234@gmail.com>
2017-10-16 20:27:18 -0700

    saving

--- node_modules/cassandra-driver/lib/auth/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/**
- * Authentication module.
- * @module auth
- */
-var baseProvider = require('./provider.js');
-exports.AuthProvider = baseProvider.AuthProvider;
-exports.Authenticator = baseProvider.Authenticator;
-exports.PlainTextAuthProvider = require('./plain-text-auth-provider.js');
