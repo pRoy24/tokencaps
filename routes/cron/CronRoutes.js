eb8e56e739ab2ef2d741ac1c4c38c5123d0cd751
TheChurchOfCrypto <roypritam1234@gmail.com>
2017-11-01 21:54:38 -0700

    fix cron job for getting daily image data

--- routes/cron/CronRoutes.js
+++ routes/cron/CronRoutes.js
@@ -19,7 +19,7 @@ module.exports = {
     DataFetchAPI.getCoinList().then(function (coinListResponse) {
       let currentCoinCounter = 0;
       new CronJob('* * * * *', function() {
-        if (currentCoinCounter <= coinListResponse.length) {
+        if (currentCoinCounter <= 100) {
           let coinSymbol = coinListResponse[currentCoinCounter].symbol;
           APIStorage.findCoinDayHistoryData(coinSymbol).then(function(apiCoinDayHistoryDataResponse){
             const coinDayHistoryResponse = apiCoinDayHistoryDataResponse.data.Data;
@@ -33,6 +33,29 @@ module.exports = {
             }
           });
           currentCoinCounter ++;
+        } else {
+          currentCoinCounter = 0;
+        }
+      }, null, true, 'America/Los_Angeles');
+
+      let allCoinCounter = 0;
+      new CronJob('* * * * *', function() {
+        if (allCoinCounter <= coinListResponse.length) {
+          let coinSymbol = coinListResponse[currentCoinCounter].symbol;
+          APIStorage.findCoinDayHistoryData(coinSymbol).then(function(apiCoinDayHistoryDataResponse){
+            const coinDayHistoryResponse = apiCoinDayHistoryDataResponse.data.Data;
+            if (coinDayHistoryResponse && coinDayHistoryResponse.length > 0) {
+              const responseData  = {};
+              responseData[coinSymbol] = coinDayHistoryResponse;
+              CoinGraph.chartCoinDailyHistoryGraph(responseData);
+              return DiskStorage.deleteCoinDayHistoryData(coinSymbol).then(function(deleteResponse){
+                return DiskStorage.saveCoinDayHistoryData(responseData);
+              });
+            }
+          });
+          allCoinCounter ++;
+        } else {
+          allCoinCounter = 0;
         }
       }, null, true, 'America/Los_Angeles');
       res.send({data: "Cron Job Started"});
--- routes/cron/index.js
+++ routes/cron/index.js
@@ -3,6 +3,6 @@ var express = require('express')
   , router = express.Router()
   , CronRoutes = require('./CronRoutes');
 
-router.route('/create-week-history-table').get(CronRoutes.createCoinDailyHistoryTable)
+router.route('/create-daily-history-table').get(CronRoutes.createCoinDailyHistoryTable)
 
 module.exports = router;
