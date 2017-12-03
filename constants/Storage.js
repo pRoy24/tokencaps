222e8c37521313184cadf41fb018a6b8dd4d5d97
TheChurchOfCrypto <roypritam1234@gmail.com>
2017-12-03 10:21:21 -0800

    remove constants

--- constants/Storage.js
+++
@@ -1,5 +0,0 @@
-// pRoy24 TokenPlex
-module.exports = {
-  REDIS_API_SERVER: "127.0.0.1", // If running remote redis server, create a REDIS host here.
-  CQL_API_SERVER: "127.0.0.1:9042"  // If running remote CQL server, create a CQL host here.
-};
--- constants/config.js
+++
@@ -1,7 +0,0 @@
-// pRoy24 TokenPlex
-// Enter your AWS S3 credentials here to upload 24 hour graph
-module.exports  = {
-  "AWS_ACCESS_KEY": "",
-  "AWS_SECRET_KEY": "",
-  "APP_SECRET_KEY": "", // Use this to set the secret that you can pass with your API requests
-}
