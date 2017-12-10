5411c1424057dc1219e33343f60bef385075ad1b
TheChurchOfCrypto <roypritam1234@gmail.com>
2017-12-10 09:04:37 -0800

    remove var from utils

--- utils/ObjectUtils.js
+++ utils/ObjectUtils.js
@@ -14,5 +14,30 @@ module.exports = {
   },
 
   writeFileToS3Location: function(coinSymbol, coinName) {
+    var AWS = require('aws-sdk'),
+      fs = require('fs');
+    const coinURI = 'public/images/charts/' + coinName + '.png';
+    // For dev purposes only
+    // Add AWS Config
+    // Read in the file, convert it to base64, store to S3
+    fs.readFile(coinURI, function (err, data) {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      const base64data = new Buffer(data, 'binary');
+      const s3 = new AWS.S3();
+      s3.upload({
+        Bucket: 'images.tokenplex.io',
+        Key: coinSymbol+".png",
+        Body: base64data,
+        ACL: 'public-read'
+      },function (resp) {
+        console.log(arguments);
+        console.log('Successfully uploaded package.');
+      });
+
+
+    });
   }
 }
